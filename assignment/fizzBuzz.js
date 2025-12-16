d0829a600cefeb35b99295e195c9031e8521e506
Greyson <greyson.frazier@gmail.com>
2025-12-16 11:47:46 -0500

    Update fizzBuzz.js

--- assignment/fizzBuzz.js
+++ assignment/fizzBuzz.js
@@ -3,10 +3,4 @@ const fizzBuzz = (number) => {
 
 };
 
-// all of the test cases:
-console.log(fizzBuzz(1));
-console.log(fizzBuzz(3));
-console.log(fizzBuzz(5));
-console.log(fizzBuzz(15));
-
 // Export the fizzBuzz function here:
